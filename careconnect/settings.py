bab887206ae2dfac0034c156588407a70f79d2be
ramashishmaurya <ramashishmaurya738@gmail.com>
2026-05-14 21:08:55 +0530

    new file structure

--- careconnect/settings.py
+++ careconnect/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 from dotenv import load_dotenv
-import os 
+import os
 
 load_dotenv()
 
@@ -26,7 +26,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
